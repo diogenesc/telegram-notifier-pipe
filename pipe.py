e15da92962304834d6f62a9debbdfd32dff19271
Diógenes Castro <diogenesxmarlon@gmail.com>
2021-08-17 18:51:29 -0300

    Altera parâmetros aceitos

--- pipe.py
+++ pipe.py
@@ -3,9 +3,9 @@ import os
 from telegram import Bot
 
 def main() -> None:
-    bot = Bot(os.environ.get('BOT_TOKEN'))
+    bot = Bot(os.environ.get('TELEGRAM_BOT_TOKEN'))
 
-    bot.send_message(chat_id=os.environ.get('CHAT_ID'), text=os.environ.get('MESSAGE_TEXT'))
+    bot.send_message(chat_id=os.environ.get('TELEGRAM_CHAT_ID'), text=os.environ.get('MESSAGE_TEXT'))
 
 
 if __name__ == '__main__':
